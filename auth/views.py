6ba4f7f4900ea7556f81a520f4c5c5f0b6349e55
schuaBob <31004842+schuaBob@users.noreply.github.com>
2020-05-21 06:30:46 +0800

    init commit with some examples

---
+++ auth/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import views, response
+# Create your views here.
+
+class AuthView(views.APIView):
+    def get(self,request):
+        res = {'data': 1, 'hello': 'world'}
+        return response.Response(res)
